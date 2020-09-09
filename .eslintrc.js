2e92963d2126688956c361acc770a037c52dfcdc
ddryo <dd.bocsfcryo@gmail.com>
2020-09-09 19:55:23 +0900

    Update dev env

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,36 @@
 module.exports = {
-    env: {
-        // browser: true,
-        // node: true,
-        // commonjs: true,
-        // es6: true,
-        // jquery: true,
-    },
-    extends: ['wordpress', 'plugin:prettier/recommended'], //eslint:recommended
-    plugins: ['react'],
-    parser: 'babel-eslint', //JSXとかでエラー出るのを回避
-    parserOptions: {
-        // ecmaVersion: 2018,
-        sourceType: 'module', //importを使うときに必要
-        ecmaFeatures: {
-            experimentalObjectRestSpread: true, //非推奨項目も注意してくれる ??
-            jsx: true,
-        },
-    },
+	extends: [ 'plugin:@wordpress/eslint-plugin/recommended' ],
+	// extends: ['wordpress', 'plugin:prettier/recommended'], //eslint:recommended
+	// parserOptions: {
+	//     sourceType: 'module', //importを使うときに必要
+	//     ecmaFeatures: {
+	//         experimentalObjectRestSpread: true, //非推奨項目も注意してくれる ??
+	//         jsx: true,
+	//     },
+	// },
 
-    rules: {
-        // 'prettier/prettier': 0,
-        indent: [2, 'tab'],
-        // quotes: ['error', 'double'],
-        // 'space-in-parens': 'error',
-        // 'comma-dangle': 'off',
-        'no-var': 'error', //varを許可しない
-        'no-console': 'off', //console.logがあってもエラーにしない
-        'require-jsdoc': 'off', //Docコメントなくてもエラーにしない
-        'valid-jsdoc': 'off', //Docコメントの書き方についてとやかくいわせない
-        camelcase: ['warn', { properties: 'never' }], //オブジェクトのキーはキャメルじゃなくてよい
-        // 'space-infix-ops': 0,
-        // 'space-in-parens': 0,
-        // 'react-in-jsx-scope': 'off',
-        // 'react/no-set-state': 'error', //react/no-set-stateルールを適用
-        'react/jsx-uses-vars': 1, //これを使うとJSXで使ってる変数がno-useとして認識されるのを防げた
-    },
+	rules: {
+		// 'prettier/prettier': 0,
+		// indent: [2, 'tab'],
+		// quotes: ['error', 'double'],
+		// 'space-in-parens': 'error',
+		// 'comma-dangle': 'off',
+		// 'no-var': 'error', //varを許可しない
+		// 'no-console': 'off', //console.logがあってもエラーにしない
+		// camelcase: ['warn', { properties: 'never' }], //オブジェクトのキーはキャメルじゃなくてよい
+
+		// jsdoc関連
+		'require-jsdoc': 0, //Docコメントなくてもエラーにしない
+		'valid-jsdoc': 0, //Docコメントの書き方についてとやかくいわせない
+		'jsdoc/require-param': 0, //Docコメントなくてもエラーにしない
+		'jsdoc/require-param-type': 0, //Docコメントの書き方についてとやかくいわせない
+		'jsdoc/check-access': 0,
+		'jsdoc/check-property-names': 0,
+		'jsdoc/empty-tags': 0,
+		'jsdoc/require-property': 0,
+		'jsdoc/require-property-description': 0,
+		'jsdoc/require-property-name': 0,
+		'jsdoc/require-property-type': 0,
+		'jsdoc/check-tag-names': 0,
+	},
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,21 @@
-/* eslint no-invalid-this: "off"*/
-
-const { src, dest, watch, series, parallel, lastRun } = require('gulp');
+const { src, dest } = require( 'gulp' );
 
 // エラー時処理
-const plumber = require('gulp-plumber'); // 続行
-const notify = require('gulp-notify'); // 通知
+const plumber = require( 'gulp-plumber' ); // 続行
+const notify = require( 'gulp-notify' ); // 通知
 
 // sass・css系
-const sass = require('gulp-sass'); // sassコンパイル
-const sassGlob = require('gulp-sass-glob'); // glob (@importの/*を可能に)
-const autoprefixer = require('gulp-autoprefixer'); // プレフィックス付与
-const gcmq = require('gulp-group-css-media-queries'); // media query整理
-const cleanCSS = require('gulp-clean-css');
-
-// webpack
-const webpack = require('webpack');
-const webpackStream = require('webpack-stream');
-const webpackConfig = require('./webpack.config');
+const sass = require( 'gulp-sass' ); // sassコンパイル
+const sassGlob = require( 'gulp-sass-glob' ); // glob (@importの/*を可能に)
+const autoprefixer = require( 'gulp-autoprefixer' ); // プレフィックス付与
+const gcmq = require( 'gulp-group-css-media-queries' ); // media query整理
+const cleanCSS = require( 'gulp-clean-css' );
 
 // JS Concat
-const babel = require('gulp-babel');
-const uglify = require('gulp-uglify');
+const babel = require( 'gulp-babel' );
+const uglify = require( 'gulp-uglify' );
 // const rename = require('gulp-rename');
-const concat = require('gulp-concat');
+// const concat = require('gulp-concat');
 
 /**
  * パス
@@ -32,7 +25,7 @@ const path = {
 		scss: 'src/scss/**/*.scss',
 		// lazysizes: 'src/js/plugins/lazysizes/*.min.js',
 		adminJs: 'src/js/admin/**/*.js',
-		js: ['src/js/**/*.js', '!src/js/plugin/*js', '!src/js/admin/*js'],
+		js: [ 'src/js/**/*.js', '!src/js/plugin/*js', '!src/js/admin/*js' ],
 		block: 'src/block/**/*.js',
 	},
 	dest: {
@@ -44,19 +37,19 @@ const path = {
 /**
  * SASSコンパイル
  */
-const compileSass = (cb) => {
-	return src(path.src.scss)
-		.pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
-		.pipe(sassGlob())
-		.pipe(sass())
+const compileScss = ( cb ) => {
+	return src( path.src.scss )
+		.pipe( plumber( { errorHandler: notify.onError( '<%= error.message %>' ) } ) )
+		.pipe( sassGlob() )
+		.pipe( sass() )
 		.pipe(
-			autoprefixer({
+			autoprefixer( {
 				cascade: false,
-			})
+			} )
 		)
-		.pipe(gcmq())
-		.pipe(cleanCSS())
-		.pipe(dest(path.dest.css));
+		.pipe( gcmq() )
+		.pipe( cleanCSS() )
+		.pipe( dest( path.dest.css ) );
 };
 
 /*
@@ -70,46 +63,24 @@ const compileSass = (cb) => {
 // };
 
 /**
- * Admin Script系のminify化
+ * Admin Script系の単純なminify化
  */
-const minifyScript = (cb) => {
-	return src(path.src.adminJs)
-		.pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
+const minifyAdminJs = ( cb ) => {
+	return src( path.src.adminJs )
+		.pipe( plumber( { errorHandler: notify.onError( '<%= error.message %>' ) } ) )
 		.pipe(
-			babel({
-				presets: ['@babel/preset-env'],
-			})
+			babel( {
+				presets: [ '@babel/preset-env' ],
+			} )
 		)
-		.pipe(uglify())
-		.on('error', function(e) {
-			console.log(e);
-		})
-		.pipe(dest(path.dest.js + '/admin'));
+		.pipe( uglify() )
+		.on( 'error', function ( e ) {
+			/* eslint no-console: 0 */
+			console.log( e );
+		} )
+		.pipe( dest( path.dest.js + '/admin' ) );
 };
 
-/**
- * Webpack
- */
-const doWebpack = (cb) => {
-	return webpackStream(webpackConfig, webpack)
-		.on('error', function(e) {
-			this.emit('end'); // webpack-streamでエラー発生時に処理を終了させずに監視を続ける
-		})
-		.pipe(dest(path.dest.js));
-};
-
-/**
- * watch
- */
-const watchFiles = (cb) => {
-	watch(path.src.scss, compileSass);
-	watch(path.src.adminJs, minifyScript);
-	// watch(path.src.jsPlugin, concatPluginScripts);
-	watch(path.src.js, doWebpack);
-	watch(path.src.block, doWebpack);
-
-	cb();
-};
-
-exports.default = watchFiles;
-// exports.lazysizes = concatPluginScripts;
+exports.compileScss = compileScss;
+exports.minifyAdminJs = minifyAdminJs;
+// exports.concatPlugins = concatPlugins;
