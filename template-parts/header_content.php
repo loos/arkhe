d34acfdce0515bec5730d057a1974fca2554e504
ddryo <dd.bocsfcryo@gmail.com>
2020-12-19 21:57:35 +0900

    フック廃止

--- template-parts/header_content.php
+++ template-parts/header_content.php
@@ -3,9 +3,7 @@
  * ヘッダー用テンプレート
  *   専用プラグインを使ってヘッダーブロックが生成されている場合はそちらを優先する
  */
-$use_header_block = apply_filters( 'arkhe_use_header_block', \Arkhe::get_plugin_data( 'use_temlate_block' ) );
-
-if ( $use_header_block ) {
+if ( \Arkhe::get_plugin_data( 'use_temlate_block' ) ) {
 
 	// ヘッダーブロックを呼び出し
 	echo do_blocks( do_shortcode( Arkhe::get_header_block_content() ) ); // phpcs:ignore
