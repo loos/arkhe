67c8cc4b2534534ae234ad2cefd800b24b1d73ac
ddryo <dd.bocsfcryo@gmail.com>
2020-12-23 22:37:15 +0900

    after_switch_theme 追加

--- inc/notice.php
+++ inc/notice.php
@@ -4,36 +4,41 @@ namespace Arkhe_Theme;
 /**
  * 管理画面へ表示するメッセージ
  */
-add_action( 'admin_notices', '\Arkhe_Theme\arkhe_theme_beta_message' );
-function arkhe_theme_beta_message() {
+add_action( 'after_switch_theme', function() {
 
-	// ベータ版アラート （1.0 で消す）
-	echo '<div class="notice notice-info -arkhe-beta"><p>' .
-		esc_html__( '"Arkhe" is currently in beta.', 'arkhe' ) . '<br>' .
-		esc_html__( 'The theme structure is subject to change significantly until the version exceeds "1.0".', 'arkhe' ) .
-	'</p></div>';
+	$theme_url = admin_url( 'themes.php?page=arkhe' );
+	echo '<div class="notice notice-info is-dismissible" style="padding-top:8px;padding-bottom:8px;">' .
+			'<p style="font-size:1.2em">' . esc_html__( 'Thank you for using "Arkhe" !', 'arkhe' ) . '</p>' .
+			'<p><a href="' . esc_url( $theme_url ) . '" class="button button-primary">' .
+				esc_html__( 'Go to the theme page', 'arkhe' ) .
+			'</a></p>' .
+		'</div>';
+} );
 
-	// 管理者にだけテーマに関する重要なお知らせを表示
-	// if ( current_user_can( 'administrator' ) ) {
+// add_action( 'admin_notices', '\Arkhe_Theme\arkhe_theme_beta_message' );
+// function arkhe_theme_beta_message() {
 
-	// 	// delete_transient( 'arkhe_notice_message' );
-	// 	$json = get_transient( 'arkhe_notice_message' );
-	// 	if ( ! $json ) {
-	// 		$response = wp_remote_get( 'https://looscdn.com/cdn/curltest/json/test.json' );
-	// 		$json     = wp_remote_retrieve_body( $response );
-	// 		set_transient( 'arkhe_notice_message', $json, 1 * DAY_IN_SECONDS );
-	// 	}
+		// 管理者にだけテーマに関する重要なお知らせを表示
+		// if ( current_user_can( 'administrator' ) ) {
 
-	// 	$notice_data = json_decode( $json, true );
+		// 	// delete_transient( 'arkhe_notice_message' );
+		// 	$json = get_transient( 'arkhe_notice_message' );
+		// 	if ( ! $json ) {
+		// 		$response = wp_remote_get( 'https://looscdn.com/cdn/curltest/json/test.json' );
+		// 		$json     = wp_remote_retrieve_body( $response );
+		// 		set_transient( 'arkhe_notice_message', $json, 1 * DAY_IN_SECONDS );
+		// 	}
 
-	// 	$message = $notice_data['message'];
-	// 	$links   = $notice_data['links'];
+		// 	$notice_data = json_decode( $json, true );
 
-	// 	echo '<div class="notice notice-info -arkhe-info">';
-	// 	echo '<p>' . esc_html( $message ) . '<br>';
-	// 	foreach ( $links as $link ) {
-	// 		echo '<a href="' . esc_url( $link['url'] ) . '">' . esc_html( $link['text'] ) . '</a>';
-	// 	}
-	// 	echo '</p></div>';
-	// }
-}
+		// 	$message = $notice_data['message'];
+		// 	$links   = $notice_data['links'];
+
+		// 	echo '<div class="notice notice-info -arkhe-info">';
+		// 	echo '<p>' . esc_html( $message ) . '<br>';
+		// 	foreach ( $links as $link ) {
+		// 		echo '<a href="' . esc_url( $link['url'] ) . '">' . esc_html( $link['text'] ) . '</a>';
+		// 	}
+		// 	echo '</p></div>';
+		// }
+// }
