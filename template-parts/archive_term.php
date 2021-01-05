51faf62f3804828946933b816a3d42c69a8047c0
ddryo <dd.bocsfcryo@gmail.com>
2021-01-05 17:35:44 +0900

    微調整

--- template-parts/archive_term.php
+++ template-parts/archive_term.php
@@ -4,17 +4,18 @@
  */
 $term_obj         = get_queried_object();
 $term_id          = $term_obj->term_id;
-$term_description = $term_obj->description;
+$term_description = apply_filters( 'arkhe_term_description', $term_obj->description, $term_id );
+$show_description = apply_filters( 'arkhe_show_term_description', ! empty( $term_description ), $term_id );
 $list_type        = apply_filters( 'arkhe_list_type_on_term', ARKHE_LIST_TYPE, $term_id );
 
 // タイトル部分
 if ( ! Arkhe::is_show_ttltop() ) {
 	Arkhe::get_part( 'archive/title', ark_get__archive_data() );
-}
 
-// 説明文
-if ( ! empty( $term_description ) ) {
-	echo '<div class="p-archive__desc">' . do_shortcode( $term_description ) . '</div>';
+	// 説明文
+	if ( $show_description ) {
+		echo '<div class="p-archive__desc">' . do_shortcode( $term_description ) . '</div>';
+	}
 }
 
 // 投稿リスト前フック
