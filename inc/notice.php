173f858dc1a51c57d8d49eda4e76ec1121e0ce07
ddryo <dd.bocsfcryo@gmail.com>
2021-01-06 22:50:03 +0900

    after_switch_theme のエラー修正

--- inc/notice.php
+++ inc/notice.php
@@ -5,14 +5,15 @@ namespace Arkhe_Theme;
  * 管理画面へ表示するメッセージ
  */
 add_action( 'after_switch_theme', function() {
-
-	$theme_url = admin_url( 'themes.php?page=arkhe' );
-	echo '<div class="notice notice-info is-dismissible" style="padding-top:8px;padding-bottom:8px;">' .
-			'<p style="font-size:1.2em">' . esc_html__( 'Thank you for using "Arkhe" !', 'arkhe' ) . '</p>' .
-			'<p><a href="' . esc_url( $theme_url ) . '" class="button button-primary">' .
-				esc_html__( 'Go to the theme page', 'arkhe' ) .
-			'</a></p>' .
-		'</div>';
+	add_action( 'admin_notices', function() {
+		$theme_url = admin_url( 'themes.php?page=arkhe' );
+		echo '<div class="notice notice-info is-dismissible" style="padding-top:8px;padding-bottom:8px;">' .
+				'<p>' . esc_html__( 'Thank you for using "Arkhe" !', 'arkhe' ) . '</p>' .
+				'<p><a href="' . esc_url( $theme_url ) . '" class="button button-primary">' .
+					esc_html__( 'Go to the theme page', 'arkhe' ) .
+				'</a></p>' .
+			'</div>';
+	} );
 } );
 
 // add_action( 'admin_notices', '\Arkhe_Theme\arkhe_theme_beta_message' );
