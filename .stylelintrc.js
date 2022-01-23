c011ffcb89afcee5e7e06aa5b2b28a3196efe6d5
ddryo <dd.bocsfcryo@gmail.com>
2022-01-23 12:46:36 +0900

    update package for stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,16 +1,21 @@
 module.exports = {
-    // plugins: ['stylelint-scss'],
+    // stylelint 14への乗り換えで必要だと書かれていたがなくても動く...？
+    // overrides: [
+    //     {
+    //       files: ["**/*.scss"],
+    //       customSyntax: "postcss-scss"
+    //     }
+    //   ],
     extends: [
         '@wordpress/stylelint-config/scss',
         'stylelint-config-rational-order',
     ],
-    ignoreFiles: ['./src/scss/foundation/base/**/*.scss', './src/js/**/*.js', './**/plugins/*.scss','./assets/**/*.css'],
+    ignoreFiles: ['./src/js/**/*.js', './**/plugins/*.scss','./assets/**/*.css'],
     rules: {
         'max-line-length': null, //max文字数を無視
         'length-zero-no-unit': null,
         // 'length-zero-no-unit': [ true, {ignore: ["custom-properties"]} ],
         'selector-class-pattern': null,
-        indentation: 4, //スペースでサイズは4
         // 'string-quotes': 'double', //ダブルクォーテーションに (wordpress でそうなってる)
         // 'no-duplicate-selectors': null, //同じセレクタの出現に関するエラーを出さない
         'function-url-quotes': 'never', //不必要なクォーテーションを禁止( これだけ自動Fixできない )
@@ -21,5 +26,5 @@ module.exports = {
         'at-rule-no-unknown': null, //scssで使える @include などにエラーがでないように
         'scss/at-rule-no-unknown': true, //scssでもサポートしていない @ルールにはエラーを出す
         // 'order/properties-alphabetical-order': true, //ABC順
-    },
+    }
 };
