16849875e1ea4af7b1493bc0719b59b3c10a505b
ddryo <dd.bocsfcryo@gmail.com>
2022-06-24 10:26:18 +0900

    ページトップボタン調整

--- template-parts/footer/fix_btns.php
+++ template-parts/footer/fix_btns.php
@@ -6,7 +6,12 @@
 if ( Arkhe::get_setting( 'show_pagetop' ) ) : ?>
 	<div class="p-fixBtnWrap">
 		<div id="pagetop" class="c-fixBtn -pagetop u-flex--c" data-onclick="pageTop" role="button" aria-label="To top">
-			<?php Arkhe::the_svg( 'chevron-up', array( 'class' => 'c-fixBtn__icon' ) ); ?>
+			<?php
+			Arkhe::the_svg( 'chevron-up', array(
+				'class' => 'c-fixBtn__icon',
+				'size'  => '20',
+			) );
+?>
 		</div>
 	</div>
 <?php
