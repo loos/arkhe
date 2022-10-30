d1f3e6b7bc57edd93ba3cf4ead8d38350aea83e7
ddryo <dd.bocsfcryo@gmail.com>
2022-10-31 02:59:42 +0900

    アイキャッチ画像部分にキャプションをフックで追記できるように

--- template-parts/single/head/thumbnail.php
+++ template-parts/single/head/thumbnail.php
@@ -2,6 +2,7 @@
 /**
  * 投稿アイキャッチ画像
  */
+$caption = isset( $args['caption'] ) ? $args['caption'] : '';
 ?>
 <figure class="p-entry__thumb">
 	<?php
@@ -10,4 +11,7 @@
 			'sizes' => '(min-width: 800px) 800px, 100vw',
 		) );
 	?>
+	<?php if ( $caption ) : ?>
+		<figcaption class="p-entry__thumb__figcaption"><?php echo wp_kses( $caption, Arkhe::$allowed_text_html ); ?></figcaption>
+	<?php endif; ?>
 </figure>
